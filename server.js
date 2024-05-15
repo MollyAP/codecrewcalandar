e46c604233056392f1780914ff7f609e011a7470
Mollyphobia <123973226+Mollyphobia@users.noreply.github.com>
2024-05-15 08:19:57 -0500

    dynamic import

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const fetch = require('node-fetch');
 const moment = require('moment');
 const cors = require('cors');
 const app = express();
@@ -10,6 +9,7 @@ app.use(cors());
 let cachedData = null;
 
 function fetchData() {
+  import('node-fetch').then(fetch => {
   fetch('https://api.jsonbin.io/v3/b/6643705aacd3cb34a847d035/latest', {
     headers: {
       'X-Master-key': '$2a$10$jt7NCradmKBFfCDVM9gHheM5tsr7BLWgofJhxOa/fFIqbgvu2dnEO'
@@ -21,6 +21,10 @@ function fetchData() {
     console.log('Data fetched:', cachedData);
   })
   .catch(error => console.error('Error fetching data:', error));
+}).catch(err => {
+  // Handle error if dynamic import fails
+  console.error('Error importing node-fetch:', err);
+});
 }
 
 // Fetch data initially
